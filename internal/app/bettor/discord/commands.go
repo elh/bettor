ced49035dbf01223c137f7e6592607a426d8463c
eugene huang <eugene@goforward.com>
2023-01-16 01:58:49 -0800

    integrate Discord /start-bet with server

--- internal/app/bettor/discord/commands.go
+++ internal/app/bettor/discord/commands.go
@@ -1,9 +1,17 @@
 package discord
 
 import (
+	"context"
+	"errors"
 	"fmt"
 
+	"github.com/bufbuild/connect-go"
 	"github.com/bwmarrin/discordgo"
+	api "github.com/elh/bettor/api/bettor/v1alpha"
+)
+
+const (
+	defaultNewUserCentipoints = 1000000 // 10K points
 )
 
 var (
@@ -11,7 +19,7 @@ var (
 
 	startBetCommand = &discordgo.ApplicationCommand{
 		Name:        "start-bet",
-		Description: "Start a new pool bet",
+		Description: "Start a new pool bet. At least 2 outcome options are required",
 		Options: []*discordgo.ApplicationCommandOption{
 			{
 				Type:        discordgo.ApplicationCommandOptionString,
@@ -37,6 +45,34 @@ var (
 				MinLength:   &one,
 				MaxLength:   1024,
 			},
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "outcome3",
+				Description: "Outcome 3",
+				MinLength:   &one,
+				MaxLength:   1024,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "outcome4",
+				Description: "Outcome 4",
+				MinLength:   &one,
+				MaxLength:   1024,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "outcome5",
+				Description: "Outcome 5",
+				MinLength:   &one,
+				MaxLength:   1024,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "outcome6",
+				Description: "Outcome 6",
+				MinLength:   &one,
+				MaxLength:   1024,
+			},
 		},
 	}
 )
@@ -49,8 +85,58 @@ func (b *Bot) StartBet(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		optionMap[opt.Name] = opt
 	}
 
+	// make sure caller user exists. if not, create a new user.
+	var bettorUserID string
+	getUserResp, err := b.Client.GetUserByUsername(context.Background(), &connect.Request[api.GetUserByUsernameRequest]{Msg: &api.GetUserByUsernameRequest{Username: i.Member.User.ID}})
+	if err != nil {
+		var connectErr *connect.Error
+		if errors.As(err, &connectErr) {
+			if connectErr.Code() == connect.CodeNotFound {
+				createUserResp, err := b.Client.CreateUser(context.Background(), &connect.Request[api.CreateUserRequest]{Msg: &api.CreateUserRequest{User: &api.User{
+					Username:    i.Member.User.ID,
+					Centipoints: defaultNewUserCentipoints,
+				}}})
+				if err != nil {
+					b.Logger.Log("msg", "failed to create user", "error", err)
+					return
+				}
+				bettorUserID = createUserResp.Msg.GetUser().GetId()
+			} else {
+				b.Logger.Log("msg", "failed to get user", "error", err)
+				return
+			}
+		} else {
+			b.Logger.Log("msg", "failed to get user", "error", err)
+			return
+		}
+	} else {
+		bettorUserID = getUserResp.Msg.GetUser().GetId()
+	}
+
+	var outcomes []*api.Outcome
+	for _, k := range []string{"outcome1", "outcome2", "outcome3", "outcome4", "outcome5", "outcome6"} {
+		if option, ok := optionMap[k]; ok {
+			outcomes = append(outcomes, &api.Outcome{
+				Title: option.StringValue(),
+			})
+		}
+	}
+	_, err = b.Client.CreateMarket(context.Background(), &connect.Request[api.CreateMarketRequest]{Msg: &api.CreateMarketRequest{Market: &api.Market{
+		Title:   optionMap["title"].StringValue(),
+		Creator: bettorUserID,
+		Type: &api.Market_Pool{
+			Pool: &api.Pool{
+				Outcomes: outcomes,
+			},
+		},
+	}}})
+	if err != nil {
+		b.Logger.Log("msg", "failed to create market", "error", err)
+		return
+	}
+
 	margs := make([]interface{}, 0, len(options))
-	msgformat := "Created! Until locked, bets are open with `/join-bet`\n"
+	msgformat := "Created! Type `/join-bet` to join the bet until it is locked.\n"
 
 	if option, ok := optionMap["title"]; ok {
 		margs = append(margs, option.StringValue())
@@ -64,6 +150,22 @@ func (b *Bot) StartBet(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		margs = append(margs, option.StringValue())
 		msgformat += "> Outcome 2: %s\n"
 	}
+	if option, ok := optionMap["outcome3"]; ok {
+		margs = append(margs, option.StringValue())
+		msgformat += "> Outcome 3: %s\n"
+	}
+	if option, ok := optionMap["outcome4"]; ok {
+		margs = append(margs, option.StringValue())
+		msgformat += "> Outcome 4: %s\n"
+	}
+	if option, ok := optionMap["outcome5"]; ok {
+		margs = append(margs, option.StringValue())
+		msgformat += "> Outcome 5: %s\n"
+	}
+	if option, ok := optionMap["outcome6"]; ok {
+		margs = append(margs, option.StringValue())
+		msgformat += "> Outcome 6: %s\n"
+	}
 
 	if err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
@@ -71,6 +173,6 @@ func (b *Bot) StartBet(s *discordgo.Session, i *discordgo.InteractionCreate) {
 			Content: fmt.Sprintf(msgformat, margs...),
 		},
 	}); err != nil {
-		b.Logger.Log("msg", "failed to register StartBet handler", "error", err)
+		b.Logger.Log("msg", "failed to InteractionRespond to StartBet", "error", err)
 	}
 }
